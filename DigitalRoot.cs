05b12d74f646912718c776e0ac3fd1dc49db50c6
SeedPaths-User <lessdismissive@email.com>
2014-04-23 13:55:28 -0600

    DigitalRootonline

---
+++ DigitalRoot.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DigitalRoot("36");
+          
+
+        }
+
+        static void DigitalRoot(string rootThis)
+        {
+            {
+                var total = 0;
+
+                for (var i = 0; i < rootThis.Length; i++)
+                {
+                    var letter = rootThis[i].ToString();
+                    var number = Convert.ToInt32(letter);
+                    total = total + number;
+                }
+                Console.WriteLine(total);
+            }
+            Console.ReadKey();
+        }
+      
+    }
+}
